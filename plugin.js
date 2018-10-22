e97905d9784dda0cf25a8fd6e1be437c5453a3db
tenglongroy <tenglongroy@gmail.com>
2018-10-23 01:16:53 +1100

    add click event listener for preview images; update style; update top bar to look like viewing PDF in Chrome. Input needs to fix, maybe get the number from "keydown" so to filter out non-numeric input.

--- plugin.js
+++ plugin.js
@@ -1,73 +1,96 @@
 var imageGalleryID = 'image-gallery-main';
 var currentImageIndex = -1;
+var imageList = undefined;
 
 
 function imageGallery(selector){
-	let imageAmount = $(selector).length;
+	imageList = $(selector);
+	let imageAmount = imageList.length;
 	$(selector).on('click', function(){
 		if(isGalleryExist()){	// container already exist, show it
-			currentImageIndex = $(selector).index($(this));
+			currentImageIndex = imageList.index($(this));
 			//showImage($(selector), $(this));
-			showImage($(selector), currentImageIndex);
+			showImage();
 			$('#'+imageGalleryID).show();
 		}
 		else{	// container not exist, need to create a new one
-			currentImageIndex = $(selector).index($(this));
+			currentImageIndex = imageList.index($(this));
 			//imageGalleryInit($(selector), $(this));
-			imageGalleryInit($(selector), currentImageIndex);
+			imageGalleryInit();
 		}
 	});
 
 }
 
 
-// index doesn't need to pass in
-function imageGalleryInit(imageList, thisObjectIndex) {
-	let imageAmount = imageList.length;
-	$('body').append("<div id='" + imageGalleryID + "'><div class='image-gallery-body'><div class='index-container'><span class='index'></span><span>/ " + imageAmount + "</span></div><div class='action-bar'><div class='action-icon action-preview'><i class='icon ig-preview'></i></div><div class='action-icon action-close'><i class='icon ig-close'></i></div></div><a class='image-gallery-nav prev'><i class='icon ig-prev'></i></a><a class='image-gallery-nav next'><i class='icon ig-next'></i></a><div class='image-gallery-display-container'> <img class='current-image'> </div></div><div class='image-gallery-preview-container'> </div></div>");
 
-	// add current image to display container
-	/*$('#'+imageGalleryID+' .current-image').attr('src', thisObject.data('display-img'));*/
-	showImage(imageList, thisObjectIndex);
+function imageGalleryInit() {
+	let imageAmount = imageList.length;
+	$('body').append("<div id='" + imageGalleryID + "'><div class='image-gallery-body'><div class='top-bar'><div class='index-container'><input class='index' type='number' value='" + currentImageIndex + "'></input><span>/ " + imageAmount + "</span></div><div class='action-bar'><div class='action-icon action-preview'><i class='icon ig-preview'></i></div><div class='action-icon action-close'><i class='icon ig-close'></i></div></div></div><a class='image-gallery-nav prev'><i class='icon ig-prev'></i></a><a class='image-gallery-nav next'><i class='icon ig-next'></i></a><div class='image-gallery-display-container'> <img class='current-image'> </div></div><div class='image-gallery-preview-container'> </div></div>");
 
 	// add click Listener for icons
 	$('#'+imageGalleryID+' .action-preview').on('click', function() {
 		$('#'+imageGalleryID).toggleClass('preview-opened');
+		isPreviewOpened = isPreviewOpened ? false: true;
 	});
 	$('#'+imageGalleryID+' .action-close').on('click', function() {
 		$('#'+imageGalleryID).hide();
 	});
 	$('#'+imageGalleryID+' .image-gallery-nav.prev').on('click', function() {
-		prevImage(imageList, thisObjectIndex);
+		prevImage();
 	});
 	$('#'+imageGalleryID+' .image-gallery-nav.next').on('click', function() {
-		nextImage(imageList, thisObjectIndex);
+		nextImage();
+	});
+	$('#'+imageGalleryID+' .index-container input.index').on('change', function() {
+		if(($(this).val()+"").match(/^\d+$/)){	//check if it is integer
+			currentImageIndex = parseInt($(this).val())-1;
+			showImage();
+		}
+		else{
+			alert('Please enter a correct number.');
+		}
 	});
 
 	// create preview container by adding preview path as image source
 	for(let i = 0; i < imageList.length; i++){
 		$('#'+imageGalleryID+' .image-gallery-preview-container').append('<div class="preview-img-wrapper"><img src="'+ imageList.eq(i).data("preview-img") +'"></div>');
 	}
+	$('#'+imageGalleryID+' .image-gallery-preview-container img').on('click', function(){
+		let clickIndex = $(this).parent().index();
+		currentImageIndex = clickIndex;
+		showImage();
+	});
+
+	showImage();
 }
 
-function prevImage(imageList, currentObjectIndex){
+function prevImage(){
 	//let currentIndex = imageList.index(currentObject);
 	//let nextIndex = (currentObjectIndex+imageList.length-1)%imageList.length;
-	thisObjectIndex = (currentObjectIndex+imageList.length-1)%imageList.length;
-	showImage(imageList, thisObjectIndex);
+	//thisObjectIndex = (currentObjectIndex+imageList.length-1)%imageList.length;
+	currentImageIndex = (currentImageIndex+imageList.length-1)%imageList.length;
+	showImage(imageList);
 }
 
-function nextImage(imageList, currentObjectIndex){
+function nextImage(){
 	//let currentIndex = imageList.index(currentObject);
 	//let nextIndex = (currentObjectIndex+1)%imageList.length;
-	thisObjectIndex = (currentObjectIndex+1)%imageList.length;
-	showImage(imageList, thisObjectIndex);
+	//thisObjectIndex = (currentObjectIndex+1)%imageList.length;
+	currentImageIndex = (currentImageIndex+1)%imageList.length;
+	showImage();
 }
 
-function showImage(imageList, thisObjectIndex){
+function showImage(){
 	// add current image to display container
-	$('#'+imageGalleryID+' .current-image').attr('src', imageList.eq(thisObjectIndex).data('display-img'));
-	$('#'+imageGalleryID+' .index-container span.index').text(thisObjectIndex+1);
+	$('#'+imageGalleryID+' .current-image').attr('src', imageList.eq(currentImageIndex).data('display-img'));
+	$('#'+imageGalleryID+' .index-container input.index').val(currentImageIndex+1);
+	activatePreview();
+}
+
+function activatePreview(){
+	$('#'+imageGalleryID+' .preview-img-wrapper').removeClass('active');
+	$('#'+imageGalleryID+' .preview-img-wrapper').eq(currentImageIndex).addClass('active');
 }
 
 
